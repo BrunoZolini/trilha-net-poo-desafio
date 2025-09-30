6739880620254fd845c0889e11a98af88baf79c6
Bruno Zolini <bruno.zolini@hotmail.com>
2025-09-30 19:23:12 -0300

    feat: smartphone project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone");
+Smartphone iphone = new Iphone("987654321", "Iphone 13", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
